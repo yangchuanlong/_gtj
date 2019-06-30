5e077153b3db4cf5ddfacaab9c8b5664c38cd99b
Administrator <yang_chuanlong@163.com>
2019-07-01 00:48:06 +0800

    openlayer demo

--- src/views/pages/dataCenter.vue
+++ src/views/pages/dataCenter.vue
@@ -1,12 +1,34 @@
 <template>
     <div class="dataCenter">
         this is data center
+        <div id="mapDiv"></div>
     </div>
 </template>
 
 <script>
+    import 'ol/ol.css';
+    import {Map, View} from 'ol';
+    import TileLayer from 'ol/layer/Tile';
+    import OSM from 'ol/source/OSM';
+
     export default {
-        name: "dataCenter"
+        name: "dataCenter",
+        mounted: function () {
+            console.log('dataCenter is mounted')
+            console.log(document.getElementById('mapDiv'));
+            new Map({
+                target: 'mapDiv',
+                layers: [
+                    new TileLayer({
+                        source: new OSM()
+                    })
+                ],
+                view: new View({
+                    center: [0, 0],
+                    zoom: 0
+                })
+            });
+        }
     }
 </script>
 
